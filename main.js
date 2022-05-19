c05e8d2378923e896b9a784e2ed8311e1c13d4db
m1s777y <multimangoost@gmail.com>
2022-05-19 21:34:32 +0700

    update site

--- main.js
+++ main.js
@@ -4,20 +4,7 @@
 //console.error("Javascript saying hello!"); указывать на ошибки(если их не решить то не запуститься код)
 //console.warn("Javascript saying hello!"); // предупреждение(все равно работать все будет)
 
-function mutilate(ap,vers,specdamage)
-{
-    console.log("Вызвался мутилейт");
-}
-if(confirm("Вызвать мутилейт?"))
-{
-    alert("красава я вызвал мутилейт");
-    mutilate(13,13,13);
-}
-else
-{
-    alert("ну и бб лох");
-}
 function onClickButton()
 {
-    alert("красава нажал")
+    alert("красава");
 }
